2e7b0162376bf632e0a6f4f5b3207a0863f08e8e
oda404 <xdxalexandru404@gmail.com>
2023-12-31 13:00:59 +0200

    config: Update default config.

--- config.def.h
+++ config.def.h
@@ -3,17 +3,18 @@
 
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
-};
-static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+	"monospace:size=10"};
+static const char *prompt = NULL; /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeNorm] = {"#bbbbbb", "#222222"},
+	[SchemeSel] = {"#eeeeee", "#005577"},
+	[SchemeOut] = {"#000000", "#00ffff"},
 };
+static unsigned int alphas[SchemeLast] = {
+	0xFF, 0xE0};
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
+static unsigned int lines = 10;
 
 /*
  * Characters not considered part of a word while deleting words
